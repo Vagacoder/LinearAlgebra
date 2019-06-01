c695f35524ce5d0a72c3b74b218d1e55fd971601
Vagacoder <qiruihu@gmail.com>
2019-06-01 12:20:37 -0700

    update 6.5

--- 65-LeastSquares.py
+++ 65-LeastSquares.py
@@ -15,3 +15,24 @@ print(A2Tb2)
 A2TA2b2 = np.concatenate((A2TA2, A2Tb2), axis=1)
 print(A2TA2b2)
 print(sp.Matrix(A2TA2b2).rref())
+print()
+
+# 4
+A4 = [[1, 3],[1, -1],[1, 1]]
+b4 = [[5],[1],[0]]
+A4T = np.transpose(A4)
+A4TA4 = np.dot(A4T, A4)
+A4Tb4 = np.dot(A4T, b4)
+print('#4')
+print(A4TA4)
+print(A4Tb4)
+A4TA4b4 = np.concatenate((A4TA4, A4Tb4), axis=1)
+print(A4TA4b4)
+print(sp.Matrix(A4TA4b4).rref())
+print()
+
+# 12
+ATAb12 = [[3, 2, -2, 13],[2, 3, 0, 14],[-2, 0, 3, -5]]
+print('#12')
+print(sp.Matrix(ATAb12).rref())
+print()
